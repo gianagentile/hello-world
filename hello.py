f1bab3f76d7fbc4b54e677987728df05235c5ab0
gianagentile <giana.gentile@student.fairfield.edu>
2026-01-28 14:45:20 -0500

    Custom hello

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print('hello world')
+#this program prompts the user for their name and greets them
+name = input("what's your name?").title()
+clean = " ".join(name.split())
+print(f"Hello, {clean}")
